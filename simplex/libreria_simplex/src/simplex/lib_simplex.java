d3231627fe0dbfaa8910e86ca93e50949eb6dda7
FelipeH22 <jfherrerap@correo.udistrital.edu.co>
2019-04-03 13:08:49 -0500

    Se añade ejercicio del metodo simplex

---
+++ simplex/libreria_simplex/src/simplex/lib_simplex.java
@@ -0,0 +1,16 @@
+package simplex;
+
+public class lib_simplex {
+    int num_filas;
+    int num_columnas;
+    int num_variables;
+    int variables[] = new int[num_variables];
+    float matriz[][] = new float[num_filas][num_columnas];
+    
+    public void transforma_ecuaciones(){
+        
+    }
+    public void crea_tabla(){
+        
+    }
+}
---
+++ simplex/src/simplex/Simplex.java
@@ -0,0 +1,11 @@
+package simplex;
+
+import simplex.*;
+public class Simplex {
+
+    public static void main(String[] args) {
+        lib_simplex lib = new lib_simplex();
+        lib.transforma_ecuaciones();
+    }
+    
+}
