1a87e4239689c4508035f429a7d6a885efbc1367
FelipeH22 <jfherrerap@correo.udistrital.edu.co>
2019-04-10 13:30:54 -0500

    Se adelanta simplex

--- simplex/libreria_simplex/src/simplex/lib_simplex.java
+++ simplex/libreria_simplex/src/simplex/lib_simplex.java
@@ -13,19 +13,33 @@ public class lib_simplex {
     ArrayList <Integer> line[];
     
     public void transforma_ecuaciones(int j, int n){
+        Integer s_matriz[][] = new Integer[j][n];
         //Iguala z a 0 
         for(i=0;i<z_temp.size();i++)
             z.add(z_temp.get(i)*-1);        
         //Añade variables s 
         for(i=0;i<j;i++)
         {
-            s.add(0);
-            s.add(i,1);
-            s.add(0);
+            for(i1=0;i1<n;i1++)
+            {
+                if(i==i1)
+                    s_matriz[i][i1]=1;
+                else
+                    s_matriz[i][i1]=0;
+            }
+        }
+        for(i=0;i<j;i++)
+        {
+            for(i1=0;i1<n;i1++)
+            {
+                s.add(s_matriz[i][i1]);
+            }
         }
             
     }
     public void crea_tabla(int j, int n){
+        int h=0;
+        int temp=n;
         line = new ArrayList[n+s.size()+2];
         for(i=0;i<n+s.size()+2;i++)
             line[i] = new ArrayList(); 
@@ -33,15 +47,33 @@ public class lib_simplex {
         {
             line[i].add(0);
         }
-        line[j].add(1);
+        line[j].add(0,1);
         for(i=0;i<j;i++)
         {
             line[i].addAll(ecuacion[i]);
-            line[i].add(s.get(i));
+            for(i1=h;i1<temp;i1++)
+                line[i].add(s.get(i1));        
             line[i].add(resultados.get(i));
+            h=h+3;
+            temp=temp+5;
         }
-        for(i=0;i<line[1].size();i++)
-            System.out.println(line[1].get(i));
+        for(i=0;i<z.size();i++)
+        {
+            line[j].add(z.get(i));
+        }
+        for(i=0;i<n;i++)
+        {
+            line[j].add(0);
+        }
+        
+        for(i=0;i<=j;i++)
+        {
+            for(i1=0;i1<line[i].size();i1++)
+            {
+                System.out.print(line[i].get(i1)+" ");
+            }
+            System.out.print("\n");
+        }
             
     }
     public void calcula(int j, int n){
