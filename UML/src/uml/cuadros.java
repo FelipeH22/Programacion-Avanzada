7d5cf5dc87764066ef2eec1c9890bfa6cb232543
FelipeH22 <jfherrerap@correo.udistrital.edu.co>
2019-05-04 19:45:43 -0500

    Listo el GUI

--- UML/src/uml/cuadros.java
+++ UML/src/uml/cuadros.java
@@ -1,5 +1,6 @@
 package uml;
 
+import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.util.ArrayList;
@@ -12,42 +13,52 @@ public class cuadros extends JPanel{
     int j;
     public ArrayList <String> clases = new ArrayList<>();
     public ArrayList <String> atributos = new ArrayList<>();
-    String clase_actual;
+    String signo;
 
     JFrame frame = new JFrame();
+    JScrollPane scroll = new JScrollPane();
     JButton back = new JButton();
     public void componentes(){
         
         frame.setSize(600,600);
         frame.setLocationRelativeTo(null);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);   
+        frame.setVisible(true);  
+        frame.add(scroll);
+        this.setLayout(null);
+        this.setPreferredSize(new Dimension(600,100));
+        scroll.setBounds(0,0,600,600);
+        scroll.setViewportView(new cuadros());
+        scroll.getViewport().setView(this);
         back.setText("Volver");
-        back.setBounds(10,10,100,20);
+        back.setBounds(10,20,100,20);
         back.setFocusable(false);
-        frame.add(back);
-        this.setBounds(0,0,600,600);
-        frame.add(new cuadros());
+        back.setVisible(true);
         
     }
     
     public void oyentes(controlador_back cb){
         back.addActionListener(cb);
     }
+    
     @Override
     public void paint(Graphics g) {
         int x=10;
         int y=70;
         Graphics2D g2d = (Graphics2D) g;	
+        this.add(back);
         for(i=0;i<controlador_fin.num_clases;i++)
         {
             g2d.drawRect(x, y, 100, 20);
             g2d.drawString(controlador_fin.clase.get(i), x+5,y+12);
-            g2d.drawString(controlador_fin.atributos.get(i),x+5,y+35);
-            g2d.drawRect(x, y+20, 100, 80);
+            g2d.drawRect(x, y+20, 100, 20);
+            g2d.drawString(controlador_fin.atributos.get(i),x+5,y+55);
+            g2d.drawRect(x, y+40, 100, 80);
             x=x+110;
         }
-            
+        this.updateUI();
+        this.setPreferredSize(new Dimension(x,y));
+        super.paintComponents(g2d);       
     }
     
 	
---
+++ UML/src/uml/evalua_abstract.java
@@ -0,0 +1,6 @@
+package uml;
+
+public abstract class evalua_abstract {
+    public abstract void determina_privacidad();
+    public abstract void evalua_herencia();
+}
