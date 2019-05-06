ba8a137f500c874bfbc506fd87edec04d1d4e4b4
FelipeH22 <jfherrerap@correo.udistrital.edu.co>
2019-05-06 18:56:23 -0500

    Adelantos

--- UML/src/uml/cuadros.java
+++ UML/src/uml/cuadros.java
@@ -51,8 +51,9 @@ public class cuadros extends JPanel{
         this.add(back);
         Graphics2D g2d = (Graphics2D) g;
         paint_clases(g2d);
+        paint_composicion(g2d);
         this.updateUI();
-        this.setPreferredSize(new Dimension(x,y));
+        this.setPreferredSize(new Dimension(x+controlador_fin.num_clases*110,y+300));
         super.paintComponents(g2d);       
     }
 
@@ -80,6 +81,43 @@ public class cuadros extends JPanel{
                 y_texto=y_texto+10;
             }
             x=x+110;
+            
+        }
+    }
+    
+    public void paint_composicion(Graphics g){
+        Graphics2D g2d = (Graphics2D) g;
+        x=10;
+        y=y+20;
+        if(this.composicion==true){
+            g2d.setColor(Color.red);
+            g2d.drawRect(x, y=y+controlador_fin.atributos[0].size()*15+20, 300, 700);
+            g2d.drawString("Composición", x+100, y+15);
+            g2d.setColor(Color.BLACK);
+            y=y+10;
+            x=x+10;
+            for(i=0;i<controlador_fin.num_clases;i++)
+            {
+                x_texto=x+4;
+                g2d.drawRect(x, y=y+20, 100, 20);
+                y_texto=y+12;
+                g2d.drawString(controlador_fin.clases[i].get(0), x_texto, y_texto);
+                g2d.drawRect(x, y=y+20, 100, controlador_fin.instancias[i].size()*15+10);   
+                y_texto=y+12;
+                for(j=0;j<controlador_fin.instancias[i].size();j++)
+                {
+                    g2d.drawString(controlador_fin.instancias[i].get(j), x_texto, y_texto);
+                    y_texto=y_texto+10;
+                }
+                g2d.drawRect(x, y=y+controlador_fin.instancias[i].size()*15+10 , 100,controlador_fin.atributos[i].size()*15+10);
+                y_texto=y+12;
+                for(j=0;j<controlador_fin.atributos[i].size();j++)
+                {
+                    g2d.drawString(controlador_fin.atributos[i].get(j), x_texto, y_texto);
+                    y_texto=y_texto+10;
+                }
+                y=y+120;
+            }
         }
     }
 	
