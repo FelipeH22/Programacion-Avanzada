053404ae8b8084b409a3d7e47e1874422c02361d
FelipeH22 <jfherrerap@correo.udistrital.edu.co>
2019-06-27 13:31:24 -0500

    Cambios

---
+++ Determinante/src/java/NewServlet.java
@@ -0,0 +1,54 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = {"/NewServlet"})
+public class NewServlet extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet NewServlet</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet NewServlet at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException {
+        int n1 = Integer.valueOf(request.getParameter("v1"));
+        int n2 = Integer.valueOf(request.getParameter("v2"));
+        int n3 = Integer.valueOf(request.getParameter("v3"));
+        int n4 = Integer.valueOf(request.getParameter("v4"));
+        
+        int resultado = (n1*n4)-(n2*n3);
+        PrintWriter sale = response.getWriter();
+        String cambio = "<span style = 'background-color:yellow;color:red;'>";
+        sale.println(cambio+" El determinante es: "+resultado+"</span>");
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
