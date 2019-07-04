214857ebcba74f58d0160bd1243f25e294215b1d
FelipeH22 <jfherrerap@correo.udistrital.edu.co>
2019-07-04 12:40:37 -0500

    Se adelanta Fútbol

--- futbol/src/java/futbol/main.java
+++ futbol/src/java/futbol/main.java
@@ -39,13 +39,99 @@ public class main extends HttpServlet {
         throws ServletException, IOException {
         String equipo1 = "COLOMBIA";
         String equipo2 = "BRASIL";
-        request.setAttribute("equipo1", "COLOMBia");
+        String equipo3 = "ARGENTINA";
+        String equipo4 = "URUGAY";
+        String equipo5 = "PARAGUAY";
+        String equipo6 = "CHILE";
+        String equipo7 = "BOLIVIA";
+        String equipo8 = "VENEZUELA";
         PrintWriter sale = response.getWriter();
-        String cambio ="<form action=\"NewServlet\" method=\"post\">\n" +
-"                           <label>"+equipo1+"</label><br>\n" +
-                            "<label>"+equipo2+"</label>\n"+
-"                           <input id=\"boton\" value=\"enviar\" type=\"submit\">\n" +
-"                        </form>";
+        String cambio = "<!DOCTYPE html>\n" +
+"\n" +
+"<html>\n" +
+"    <head>\n" +
+"        <title>Determinante</title>\n" +
+"        <meta charset=\"UTF-8\">\n" +
+"        <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n" +
+"        <style>\n" +
+"            body{\n" +
+"                font-family: \"courier\";\n" +
+"            }\n" +
+"            table{\n" +
+"                border-color: blue;\n" +
+"            }\n" +
+"            td{\n" +
+"                border-color: white;\n" +
+"            }\n" +
+"            input{\n" +
+"                border-color: red;\n" +
+"            }\n" +
+"            #boton{\n" +
+"                margin-top: 13px;            \n" +
+"            }\n" +
+"            \n" +
+"        </style>\n" +
+"    </head>\n" +
+"    <body>\n" +
+"        <h2>Copa América</h2>\n" +
+"        <div>\n"
+                + "<form action=\"NewServlet\" method=\"post\"> \n" +
+"                           <table border=\"1\"> \n" +
+"                                <tbody> \n" +
+"                                    <tr> \n" +
+"                                        <td>"+equipo1+"</td> \n" +
+"                                        <td><input type=\"number\" name=\"v1\"></td> \n" +
+"                                    </tr> \n" +
+"                                    <tr> \n" +
+"                                        <td>"+equipo2+"</td> \n" +
+"                                        <td><input type=\"number\" name=\"v2\"></td> \n" +
+"                                    </tr> \n" +
+"                                </tbody> \n" +
+"                            </table>\n" +
+"                           <table border=\"1\"> \n" +
+"                                <tbody> \n" +
+"                                    <tr> \n" +
+"                                        <td>"+equipo3+"</td> \n" +
+"                                        <td><input type=\"number\" name=\"v3\"></td> \n" +
+"                                    </tr> \n" +
+"                                    <tr> \n" +
+"                                        <td>"+equipo4+"</td> \n" +
+"                                        <td><input type=\"number\" name=\"v4\"></td> \n" +
+"                                    </tr> \n" +
+"                                </tbody> \n" +
+"                            </table> \n" +
+"                            <table border=\"1\"> \n" +
+"                                <tbody> \n" +
+"                                    <tr> \n" +
+"                                        <td>"+equipo5+"</td> \n" +
+"                                        <td><input type=\"number\" name=\"v5\"></td> \n" +
+"                                    </tr> \n" +
+"                                    <tr> \n" +
+"                                        <td>"+equipo6+"</td> \n" +
+"                                        <td><input type=\"number\" name=\"v6\"></td> \n" +
+"                                    </tr> \n" +
+"                                </tbody> \n" +
+"                            </table> \n" +
+"                            <table border=\"1\"> \n" +
+"                                <tbody> \n" +
+"                                    <tr> \n" +
+"                                        <td>"+equipo7+"</td> \n" +
+"                                        <td><input type=\"number\" name=\"v7\"></td> \n" +
+"                                    </tr> \n" +
+"                                    <tr> \n" +
+"                                        <td>"+equipo8+"</td> \n" +
+"                                        <td><input type=\"number\" name=\"v8\"></td> \n" +
+"                                    </tr> \n" +
+"                                </tbody> \n" +
+"                            </table> \n" +
+"                               <input id=\"boton\"type=\"submit\" value=\"Confirmar\"></td>\n" +
+"                \n" +
+"                        </form>" +
+                
+"        </div>\n" +
+"    </body>\n" +
+"</html>\n" +
+"";
         sale.println(cambio);
     }
 
